2e015c8dd5e47f9a4dae0f566c558f1d8aa837ba
Hanae 800 <hanaehmimid@gmail.com>
2019-09-18 20:06:17 -0400

    Modification 2

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -11,3 +11,4 @@ adjectives = ["gargantuan","gaseous","hairy","damaged","baggy","bare","eager"]
 #print(random.choice(adjectives))
 
 print("There was once a boy named" + random.choice(names) + ". He lived in the jungle, and his best friend was a" + random.choice(nouns) + ". He tried his hardest to impress his friend by learning how to run a python program, but unfortunately there were no" + random.choice(nouns) + "in the jungle. So," + random.choice(names) + "had to come up with a different idea of how to run his python program. He walked for days and days to the nearest lake," + random.choice(names) + ", to find a shop where he could buy a python." + random.choice(names) + "returned to the jungle to his friend the lobster with his new python. His friend looked at him and said, “I thought python was a computer program, not a" + random.choice(nouns) + ".")
+print("Agony" + random.choice(nouns) +"Tarantino is a great director but he can be annoying sometimes" )
