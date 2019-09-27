8966e0e927d14a139bbb4025ad677b5de9370ea3
Hanae 800 <hanaehmimid@gmail.com>
2019-09-27 18:33:59 -0400

    Final

---
+++ Exrecice-Week 3.py
@@ -0,0 +1,18 @@
+
+#%%
+#Keyword: def
+def add( value1 , value2 , value3 ):
+    return value1 + value2 + value3
+
+
+#%%
+#Keyword: def
+def add( value1 , value2 , value3 ):
+    return value1 + value2 + value3
+
+Value1 = 1
+Value2 = 6
+value3 = 4
+print(add(1,6,4))
+
+#%%
--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,14 @@
 print("Hello World")
 print("I'm excited to learn to code! My Undergrad left me bias against Python, but I'm hoping to finally learn!")
+# Greet the user
+print ("Hi")
+
+name = input ("what's your name?") #ask the user their name 
+print("It's nice to meet you", name)
+
+answer = input("Are you enjoying the course?")
+
+if answer == "yes":
+    print("That's good to hear!")
+else:
+     print("Oh no! That makes me sad!")
